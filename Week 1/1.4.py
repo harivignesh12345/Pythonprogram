5f74e1f7d9407b67cfe8d5d5f607c073411dddd7
harivignesh12345 <231001051@rajalakshmi.edu.in>
2024-06-15 21:46:20 +0530

    Create 1.4.py

---
+++ Week 1/1.4.py
@@ -0,0 +1,59 @@
+Alfred buys an old scooter for Rs. X and spends Rs. Y on its repairs. If he sells the scooter for Rs. Z (Z>X+Y). Write a program to help Alfred to find his gain percent. Get all the above-mentioned values through the keyboard and find the gain percent.
+
+Input Format:
+
+The first line contains the Rs X
+
+The second line contains Rs Y
+
+The third line contains Rs Z
+
+Sample Input:
+
+10000
+
+250
+
+15000
+
+Sample Output:
+
+46.34 is the gain percent.
+
+
+
+For example:
+
+Input	Result
+45500
+500
+60000
+30.43 is the gain percent.
+Answer:(penalty regime: 0 %)
+d=((c-(a+b))/(a+b))*100
+print("%.2f"%d,"is the gain percent.")
+
+
+ 
+Feedback
+Input	Expected	Got	
+10000
+250
+15000
+46.34 is the gain percent.
+46.34 is the gain percent.
+45500
+500
+60000
+30.43 is the gain percent.
+30.43 is the gain percent.
+5000
+0
+7000
+40.00 is the gain percent.
+40.00 is the gain percent.
+12500
+5000
+18000
+2.86 is the gain percent.
+2.86 is the gain percent.
