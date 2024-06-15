cffa58d6b71f2156ef34a4adb27241d86133c60b
harivignesh12345 <231001051@rajalakshmi.edu.in>
2024-06-15 22:11:09 +0530

    Create 2.10.py

---
+++ Week 2/2.10.py
@@ -0,0 +1,23 @@
+An online retailer sells two products: widgets and gizmos. Each widget weighs 75 grams. Each gizmo weighs 112 grams. Write a program that reads the number of widgets and the number of gizmos from the user. Then your program should compute and display the total weight of the parts.
+
+Sample Input:
+
+10
+
+20
+
+Sample Output:
+
+The total weight of all these widgets and gizmos is 2990 grams.
+
+a=int(input())
+b=int(input())
+c=a*75
+d=b*112
+e=c+d
+print(f"The total weight of all these widgets and gizmos is {e} grams.")
+Input	Expected	Got	
+10
+20
+The total weight of all these widgets and gizmos is 2990 grams.
+The total weight of all these widgets and gizmos is 2990 grams.
