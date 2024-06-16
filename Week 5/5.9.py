8e01df65280afc69fb7488936242e4a9aec8f027
harivignesh12345 <231001051@rajalakshmi.edu.in>
2024-06-16 07:23:06 +0530

    Create 5.9.py

---
+++ Week 5/5.9.py
@@ -0,0 +1,32 @@
+Write a python to read a sentence and print its longest word and its length
+
+
+For example:
+
+Input	Result
+This is a sample text to test
+sample
+6
+n=input()
+p=n.split()
+
+a=max(p,key=len)
+print(a)
+x=len(a)
+print(x)
+Input	Expected	Got	
+This is a sample text to test
+sample
+6
+sample
+6
+Rajalakshmi Engineering College, approved by AICTE
+Rajalakshmi
+11
+Rajalakshmi
+11
+Cse IT CSBS MCT
+CSBS
+4
+CSBS
+4
